a177ef307db1576d81d8b7be0b7a31ce8a95a65c
Charan Kumar Timmidi <117446916+Charankumar017@users.noreply.github.com>
2024-06-18 21:28:04 +0530

    Removed download option in react video player

--- src/pages/components/VideoPlayer.js
+++ src/pages/components/VideoPlayer.js
@@ -14,6 +14,13 @@ const VideoPlayer = ({ url, width = "100%", height = "220px" }) => {
       onPlay={() => {
         setPlay(true);
       }}
+      config={{
+        file: {
+          attributes: {
+            controlsList: "nodownload",
+          },
+        },
+      }}
     />
   );
 };
