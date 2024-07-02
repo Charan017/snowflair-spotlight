d90fafc6f221f60f8d3f2859c2bbc5c798ef5cd6
Charan Kumar Timmidi <117446916+Charankumar017@users.noreply.github.com>
2024-07-02 07:52:51 +0530

    Updated the video player to client requested changes

--- src/pages/components/VideoPlayer.js
+++ src/pages/components/VideoPlayer.js
@@ -1,25 +1,8 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef } from "react";
 import ReactPlayer from "react-player";
-import CustomImage from "./CustomImage";
-import React from "react";
 
 const VideoPlayer = ({ url, width = "100%", height = "220px" }) => {
   const playerRef = useRef(null);
-  const [isFullscreen, setIsFullscreen] = useState(false);
-  const [isMuted, setIsMuted] = useState(true);
-  const [showControls, setShowControls] = useState(false);
-
-  const handleShowControls = (e) => {
-    setShowControls(!showControls);
-  };
-
-  const handleMouseEnter = () => {
-    setShowControls(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowControls(false);
-  };
 
   const handleEnded = () => {
     if (playerRef.current) {
@@ -27,72 +10,19 @@ const VideoPlayer = ({ url, width = "100%", height = "220px" }) => {
     }
   };
 
-  // const toggleFullscreen = () => {
-  //   if (!document.fullscreenElement) {
-  //     if (playerRef.current.wrapper) {
-  //       playerRef.current.wrapper.requestFullscreen();
-  //       setIsFullscreen(true);
-  //     }
-  //   } else if (document.fullscreenElement) {
-  //     document.exitFullscreen();
-  //     setIsFullscreen(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const handleClick = () => {
-  //     toggleFullscreen();
-  //   };
-
-  //   const videoElement = playerRef.current.getInternalPlayer();
-  //   videoElement.addEventListener("click", handleClick);
-
-  //   return () => {
-  //     videoElement.removeEventListener("click", handleClick);
-  //   };
-  // }, [isFullscreen]);
-
   return (
-    <div
-      className="relative"
-      onMouseOver={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <ReactPlayer
-        ref={playerRef}
-        width={width}
-        height={height}
-        url={url}
-        playing
-        loop
-        muted={isMuted}
-        onEnded={handleEnded}
-      />
-      {showControls && (
-        <div className="absolute right-2 bottom-1 flex justify-end">
-          {isMuted ? (
-            <div
-              className="flex items-center justify-center"
-              onClick={(e) => {
-                setIsMuted(false);
-              }}
-            >
-              <CustomImage src={"/mute.svg"} width={20} height={20} />
-            </div>
-          ) : (
-            <div
-              style={{ color: "white" }}
-              className="flex items-center justify-center"
-              onClick={(e) => {
-                setIsMuted(true);
-              }}
-            >
-              <CustomImage src={"/unMute.svg"} width={20} height={20} />
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+    <ReactPlayer
+      ref={playerRef}
+      width={width}
+      height={height}
+      url={url}
+      playing
+      loop
+      muted
+      onEnded={handleEnded}
+      controls
+      config={{ file: { attributes: { controlsList: "nodownload" } } }}
+    />
   );
 };
 
